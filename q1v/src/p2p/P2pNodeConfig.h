1e5a76e1a290ed2778888e2679622ff748046730
JamilServices <jamilservicos@gmail.com>
2024-04-28 10:54:51 -0300

    feat ✨: Implement a Peer-to-Peer (P2P) networking system, which allows nodes to communicate with each other and share information.

---
+++ q1v/src/p2p/P2pNodeConfig.h
@@ -0,0 +1,69 @@
+/**
+ * Copyright (c) 2012-2016, The CryptoNote developers, The Bytecoin developers
+ * Copyright (c) 2021-2024, The Quan Project developers
+ *
+ * Complete copyright information can be found in the accompanying LICENSE file
+*/
+
+#pragma once
+
+#include <chrono>
+#include "NetNodeConfig.h"
+
+namespace Q1v {
+
+    class P2pNodeConfig : public NetNodeConfig {
+    public:
+        P2pNodeConfig();
+
+        // getters
+        std::chrono::nanoseconds getTimedSyncInterval() const;
+
+        std::chrono::nanoseconds getHandshakeTimeout() const;
+
+        std::chrono::nanoseconds getConnectInterval() const;
+
+        std::chrono::nanoseconds getConnectTimeout() const;
+
+        size_t getExpectedOutgoingConnectionsCount() const;
+
+        size_t getWhiteListConnectionsPercent() const;
+
+        boost::uuids::uuid getNetworkId() const;
+
+        size_t getPeerListConnectRange() const;
+
+        size_t getPeerListGetTryCount() const;
+
+        // setters
+        void setTimedSyncInterval(std::chrono::nanoseconds interval);
+
+        void setHandshakeTimeout(std::chrono::nanoseconds timeout);
+
+        void setConnectInterval(std::chrono::nanoseconds interval);
+
+        void setConnectTimeout(std::chrono::nanoseconds timeout);
+
+        void setExpectedOutgoingConnectionsCount(size_t count);
+
+        void setWhiteListConnectionsPercent(size_t percent);
+
+        void setNetworkId(const boost::uuids::uuid &id);
+
+        void setPeerListConnectRange(size_t range);
+
+        void setPeerListGetTryCount(size_t count);
+
+    private:
+        std::chrono::nanoseconds timedSyncInterval;
+        std::chrono::nanoseconds handshakeTimeout;
+        std::chrono::nanoseconds connectInterval;
+        std::chrono::nanoseconds connectTimeout;
+        boost::uuids::uuid networkId;
+        size_t expectedOutgoingConnectionsCount;
+        size_t whiteListConnectionsPercent;
+        size_t peerListConnectRange;
+        size_t peerListGetTryCount;
+    };
+
+}
