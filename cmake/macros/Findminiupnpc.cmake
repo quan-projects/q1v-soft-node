6906382c6a63ce2f12eb871dddf9ae8d25610dfe
Gervasio Junior <jamilservicos@gmail.com>
2024-05-06 10:28:05 -0300

    BREAKING CHANGE: v1.0.0 (#7)
    * V1.0.0 (#5)
    
    * feat ✨: Ignore specific files and dirs in .gitignore
    
    * feat ✨: Add governance, contributor guidelines and maintainers
    
    * feat ✨: Add GPL license and copyright information for legal compliance.
    
    * feat ✨: Initial commit creating a new VERSION file with version number 1.0.0.
    
    * docs 📝: Add README
    
    * BREAKING CHANGE: Remove olds

--- cmake/macros/Findminiupnpc.cmake
+++
@@ -1,7 +0,0 @@
-include_directories(${CMAKE_CURRENT_SOURCE_DIR}/q1v/external/miniupnpc-2.2.7/lib/cmake)
-
-find_package (miniupnpc REQUIRED PATHS ${CMAKE_CURRENT_SOURCE_DIR}/q1v/external/miniupnpc-2.2.7 NO_DEFAULT_PATH)
-
-if (TARGET miniupnpc::miniupnpc)
-    message(STATUS "libminiupnpc loaded")
-endif()
