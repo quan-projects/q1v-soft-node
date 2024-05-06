70bd5b5729269478d9098516f7d9d515c062fa4f
JamilServices <jamilservicos@gmail.com>
2024-05-06 10:25:20 -0300

    BREAKING CHANGE: Remove olds

--- q1v/src/cryptoNoteCore/ITimeProvider.cpp
+++
@@ -1,8 +0,0 @@
-/**
- * Copyright (c) 2012-2016, The CryptoNote developers, The Bytecoin developers
- * Copyright (c) 2021-2024, The Quan Project developers
- *
- * Complete copyright information can be found in the accompanying LICENSE file
-*/
-
-#include "ITimeProvider.h"
